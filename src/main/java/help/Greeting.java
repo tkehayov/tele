754b33710b9f780d5024c72a39bd4dbe5da122fc
kehayov <tihomirkehayov@deltasource.eu>
2017-01-23 16:56:02 +0200

    init commit

---
+++ src/main/java/help/Greeting.java
@@ -0,0 +1,11 @@
+package help;
+
+public class Greeting {
+	public final long id;
+    public final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
