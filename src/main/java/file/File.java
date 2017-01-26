4c05834696325f455e648d5a5d718af440c9ee69
kehayov <tihomirkehayov@deltasource.eu>
2017-01-26 16:08:46 +0200

    read file from dir

---
+++ src/main/java/file/File.java
@@ -0,0 +1,8 @@
+package file;
+
+public class File {
+	public String print(String str) {
+		System.out.println(str);
+		return "Asdf";
+	}
+}
---
+++ src/main/java/file/FileRoute.java
@@ -0,0 +1,14 @@
+package file;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("file:/tmp/cim/?noop=true").bean(File.class, "print(String)");
+	}
+
+}
