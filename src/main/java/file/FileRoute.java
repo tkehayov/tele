bf7341745b2a92ed148f54b955625f8aa9211de2
kehayov <tihomirkehayov@deltasource.eu>
2017-01-26 17:08:38 +0200

    add heade

--- src/main/java/file/FileRoute.java
+++ src/main/java/file/FileRoute.java
@@ -1,6 +1,9 @@
 package file;
 
+import java.beans.Expression;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.FileComponent;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +11,8 @@ public class FileRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("file:/tmp/cim/?recursive=true&noop=true").bean(File.class, "print(String)");
+		from("file:/tmp/cim/?recursive=true&noop=true")
+				.bean(File.class, "print(String)").setHeader("ContentProvider", constant("hell"));
 	}
 
 }
